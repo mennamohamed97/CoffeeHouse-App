5d425e7b7aff08e04d507df0a9287ce588aaadd3
menna moahamed <96254138+mennamohamed97@users.noreply.github.com>
2024-05-02 20:30:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Coffee App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.brown,
